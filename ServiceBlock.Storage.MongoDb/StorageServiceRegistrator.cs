8355f77fdd7e6b01c86be0a43a91958b4f604ffd
Zrean Tofiq <zrean.tofiq@netlight.com>
2020-02-16 00:34:37 +0100

    Move mongodb implementation to separate pacakge

--- ServiceBlock.Storage.MongoDb/StorageServiceRegistrator.cs
+++ ServiceBlock.Storage.MongoDb/StorageServiceRegistrator.cs
@@ -3,16 +3,16 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using ServiceBlock.Interface;
-using ServiceBlock.Storage.Options;
+using ServiceBlock.Storage.MongoDb.Options;
 
-namespace ServiceBlock.Storage
+namespace ServiceBlock.Storage.MongoDb
 {
     public class StorageServiceRegistrator : IServiceConfiguration
     {
         public void Registrations(IServiceCollection services, IConfiguration config)
         {
             var section = config.GetSection(nameof(MongoDb));
-            services.Configure<MongoDb>(section);
+            services.Configure<Options.MongoDb>(section);
         }
 
         public void WarmUp(IServiceProvider services)
